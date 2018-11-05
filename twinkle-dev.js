92ec8ee9e926d68507d02d9afc4a669b6e5c604d
Wetitpig <winsto003@hotmail.com>
2018-11-05 10:19:50 +0800

    Do not use push in twinkle-dev.js

--- twinkle-dev.js
+++ twinkle-dev.js
@@ -1,4 +1,4 @@
-// 不要importScript User:Wetittwinkle/twinkle-dev.js！可能出現未預期的危害。
+// importScript User:Wetittwinkle/twinkle-dev.js，後果自負。
 
 (function () {
     var PREFIX = 'User:Wetittwinkle/';
@@ -20,6 +20,7 @@
     //    $('#simpleSearch input[type="search"]').attr('placeHolder', text);
     };
 
+<<<<<<< HEAD
     tests.push('morebits.js');
     tests.push('twinkle.js');
     tests.push('modules/twinklearv.js');
@@ -67,5 +68,55 @@
             message('Twinkle Done');
         });
     });
+=======
+	var tests = ['morebits.js',
+	'twinkle.js',
+	'modules/twinklearv.js',
+	'modules/twinklewarn.js',
+	'modules/friendlyshared.js',
+	'modules/friendlytag.js',
+	'modules/friendlytalkback.js',
+	'modules/twinklebatchdelete.js',
+	'modules/twinklebatchundelete.js',
+	'modules/twinkleblock.js',
+	'modules/twinkleclose.js',
+	'modules/twinkleconfig.js',
+	'modules/twinklecopyvio.js',
+	'modules/twinkledelimages.js',
+	'modules/twinklediff.js',
+	'modules/twinklefluff.js',
+	'modules/twinkleimage.js',
+	'modules/twinkleprotect.js',
+	'modules/twinklespeedy.js',
+	'modules/twinkleunlink.js',
+	'modules/twinklexfd.js'
+	];
+
+	mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.RegExp', 'mediawiki.Title', 'jquery.ui.dialog', 'jquery.tipsy']).done(function () {
+		mw.loader.load('https://zh.wikipedia.org/w/index.php?title=User:Wetittwinkle/morebits.css&action=raw&ctype=text/css', 'text/css');
+
+		var i=0;
+		var finished = 0;
+		var code = [];
+
+		// all
+		message('Loading WTW...');
+		var promises = [];
+		var done = function (x) {
+			return function (data) {
+				finished++;
+				//message('Loading WTW... (' + finished + '/' + tests.length + ')');
+				code[x] = data;
+			};
+		};
+		for (i=0; i<tests.length; i++) {
+			promises[i] = load(tests[i]).done(done(i));
+		}
+		$.when.apply($, promises).done(function () {
+			eval(code.join('\n;\n'));
+			message('Twinkle Done');
+		});
+	});
+>>>>>>> 550ee08... Do not use push in twinkle-dev.js
 })();
 

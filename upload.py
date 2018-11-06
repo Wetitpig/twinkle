3a7912965cc390c8da01715b30f658eb19d8a917
Wetitpig <winsto003@hotmail.com>
2018-11-06 16:32:18 +0800

    Minify both

--- upload.py
+++ upload.py
@@ -7,6 +7,7 @@ from subprocess import Popen, PIPE
 from time import sleep, time
 from hashlib import sha256
 from getpass import getpass
+import rjsmin, rcssmin
 
 site = SetSite('zh.wikipedia.org')
 
@@ -22,25 +23,33 @@ def list():
 		cmd = Popen(["find . \( -name '*.js' -o -name '*.css' \) | sed 's|^./||'"], shell=True, stdout=PIPE)
 	else:
 		cmd = Popen(["find . \( -name '*.js' -o -name '*.css' \) -printf '%P\n'"], shell=True, stdout=PIPE)
-	files = cmd.communicate()[0].split('\n')
-	files = files[:-1]
+	files = cmd.communicate()[0].split('\n')[:-1]
 	print "Number of Files Found: %s" % str(len(files))
 	return files
 
 def upload(username, file):
 	start = time()
 	page = site.Pages["User: %s/%s" % (username, file)]
+
 	fp = open(file, "r")
-	text = fp.read()[:-1].rstrip()
+	text = fp.read()
 	fp.close()
+
+	if file.find(".js") != -1:
+		text = rjsmin.jsmin(text)
+		text = "// <nowiki>\n" + text + "\n// </nowiki>"
+	elif file.find(".css") != -1:
+		text = rcssmin.cssmin(text)
+
 	if sha256(page.text().encode("utf-8")).hexdigest() == sha256(text).hexdigest():
 		print "%s unchanged" % file
 		return 0
+
 	comment = "更新Twinkle至Gitlab最新版本。"
 	page.save(text.decode("utf-8"), summary=comment, bot=False)
 	print "Uploaded %s" % file
 	end = time()
-	return 20 + start - end
+	return 15 + start - end
 
 def main(username, password, files):
 	login(username, password)
